948d7ad1f55e8f0223ecdfe0ebbc4f9cef06b934
Brandon Sueur <gynidark@gmail.com>
2025-10-12 10:29:54 +0200

    feat: push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,43 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./component/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        primary: "#18306C",
-        secondary: "#2B61EA",
-      },
-      fontFamily: {
-        airbnb: ["var(--font-airbnb)", "Airbnb Cereal", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
-};
+/** @type {import('tailwindcss').Config} *//** @type {import('tailwindcss').Config} */
+
+export default {module.exports = {
+
+  content: [  content: [
+
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./component/**/*.{js,ts,jsx,tsx,mdx}",    "./component/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+  ],  ],
+
+  darkMode: "class",  darkMode: "class",
+
+  theme: {  theme: {
+
+    extend: {    extend: {
+
+      colors: {      colors: {
+
+        primary: "#18306C",        primary: "#18306C",
+
+        secondary: "#2B61EA",        secondary: "#2B61EA",
+
+      },      },
+
+      fontFamily: {      fontFamily: {
+
+        airbnb: ["var(--font-airbnb)", "Airbnb Cereal", "sans-serif"],        airbnb: ["var(--font-airbnb)", "Airbnb Cereal", "sans-serif"],
+
+      },      },
+
+    },    },
+
+  },  },
+
+  plugins: [],  plugins: [],
+
+};};
